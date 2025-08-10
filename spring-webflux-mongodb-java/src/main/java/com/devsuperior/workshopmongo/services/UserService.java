f9ff009627c3db56915d5141fbef263729ff1273
Josaf4 <josafa.soares2909@gmail.com>
2025-08-10 17:28:11 -0400

    GET all users, user by id.

--- spring-webflux-mongodb-java/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ spring-webflux-mongodb-java/src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -1,16 +1,14 @@
 package com.devsuperior.workshopmongo.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.dto.UserDTO;
-import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
@@ -18,6 +16,15 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	public Flux<UserDTO> findAll() {
+		return repository.findAll().map(user -> new UserDTO(user));
+	}
+
+	public Mono<UserDTO> findById(String id) {
+		return repository.findById(id)
+				.map(existingUser -> new UserDTO(existingUser))
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
+	}
 	/*
 	@Transactional(readOnly = true)
 	public List<UserDTO> findAll() {
